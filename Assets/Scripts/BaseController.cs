502c8f5be811a25c5406b021c42db64b85acf5a5
adelipingu14 <adelipingu14@gmail.com>
2025-10-24 20:37:16 +0900

    Fix : BaseController - Made needed Methods for BaseController

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -5,4 +5,48 @@ using UnityEngine;
 public class BaseController : MonoBehaviour
 {
     protected Rigidbody _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected Vector2 movementDirection = Vector2.zero;
+    public Vector2 MovementDirection => movementDirection;
+
+    protected Vector2 lookDirection = Vector2.zero;
+    public Vector2 LookDirection => lookDirection;
+
+
+    protected virtual void Awake()
+    { 
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+
+    protected virtual void FixedUpdate()
+    {
+
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    private void Movement(Vector2 direction)
+    {
+
+    }
+
+    private void Rotate(Vector2 direction)
+    {
+
+    }
 }
