411605aeba8ce9ce99c40c7c18c88968f41579d0
adelipingu14 <adelipingu14@gmail.com>
2025-10-24 21:06:58 +0900

    Fix : Add scripts for Player Movement Controll (test version)

--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -27,26 +27,32 @@ public class BaseController : MonoBehaviour
 
     protected virtual void Update()
     {
-
+        HandleAction();
+        Rotate(lookDirection);
     }
 
     protected virtual void FixedUpdate()
     {
-
+        Movement(movementDirection);
     }
 
     protected virtual void HandleAction()
     {
-
+        
     }
 
     private void Movement(Vector2 direction)
     {
+        direction = direction * 8;
 
+        _rigidbody.velocity = new Vector3(direction.x, direction.y, 0);
     }
 
     private void Rotate(Vector2 direction)
     {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f;
 
+        characterRenderer.flipX = isLeft;
     }
 }
