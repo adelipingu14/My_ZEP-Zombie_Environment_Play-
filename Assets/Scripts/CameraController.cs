5d751aa630b76c651c4aefd227af8fce39d20723
adelipingu14 <adelipingu14@gmail.com>
2025-10-26 22:23:05 +0900

    Fix : CameraController - Make the 'Main Camera' follow the 'Player' Object

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform target;
+    float offsexX;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (target == null)
+            return;
+
+        offsexX = transform.position.x - target.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            return;
+
+        Vector3 pos = transform.position;
+        pos.x = target.position.x + offsexX;
+        transform.position = pos;
     }
 }
